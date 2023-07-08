bba89061e57e63a998ae472dd1759694d6532f5e
Eric David Smith <erictherobot@gmail.com>
2023-07-08 10:55:07 -0400

    Update posts.rs

--- api/posts.rs
+++ api/posts.rs
@@ -1,4 +1,6 @@
-use actix_web::{web, HttpResponse, Responder};
+// Example: http://localhost:8080/api/posts
+
+use actix_web::{ web, HttpResponse, Responder };
 
 pub async fn posts() -> impl Responder {
     HttpResponse::Ok().body("This is the posts API endpoint")
